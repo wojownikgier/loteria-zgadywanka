bcedb157061a7663cd769b656a8efb7978666a82
wojownikgier <kacperistelskixd@gmail.com>
2024-09-04 13:44:19 +0200

    trzecia pruba

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("witam panie");
         System.out.println("lekcja programowaniea");
+
+        double wylosowana = (int)(Math.random()*10+1);
+        System.out.println(wylosowana);
+        //git init , git config  user.name user.email , git commit -m "jakas nazwa", git remote add origin , git push - u master
     }
 }

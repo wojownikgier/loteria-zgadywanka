2715bd48b02db20c34ff3ee4b14886901b3a6739
wojownikgier <kacperistelskixd@gmail.com>
2024-09-05 09:03:42 +0200

    cos tam

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println("witam panie");
         System.out.println("lekcja programowaniea");
 
-        int wylosowana = (int)(Math.random()*10+1);//losuje liczbe
+        int wylosowana = (int)(Math.random()*100+1);//losuje liczbe
         System.out.println(wylosowana);
 
         //git init , git config  user.name user.email , git commit -m "jakas nazwa", git remote add origin ,
@@ -22,16 +22,66 @@ public class Main {
         typy złożone zawsze pisane wielką literą
         obiekty które mją wląsności i metody
          */
-        System.out.println("zgadnij liczbe zakresu 1-10");
+        System.out.println("zgadnij liczbe zakresu 1-100");
         Scanner klawiatura = new Scanner(System.in);
-        int wpisanaliczba = klawiatura.nextInt();//wczytywamoe z klawiatury
+        int wpisanaliczba = klawiatura.nextInt();//wczytywanie z kawiatury liczby całkowitej
         System.out.println("wpisano z klawiatury "+wpisanaliczba);
-        if (wpisanaliczba == wylosowana) {//muwi to czy wpisałes poprawna liczbe
+        if (wpisanaliczba == wylosowana) {//instrukcja warunkowa
             System.out.println("trafiona liczba");
         }else {
             System.out.println("Nie trafiono sprubuj ponownie");
         }
-        String komunikat = wylosowana == wpisanaliczba ? "poprawnie":"niepoprawnie";//wypisuje text
+        String komunikat = wylosowana == wpisanaliczba ? "poprawnie":"niepoprawnie";//wyrażenie warunkowe
         System.out.println(komunikat);
+
+        int roznica = wylosowana>wpisanaliczba? wylosowana:wpisanaliczba-wylosowana;
+        roznica = roznica/10; // dzielenie całkowite bo winik  typu int
+        switch (roznica){
+            case 0:
+                System.out.println("brawo good");
+                break;
+            case 1:
+                System.out.println("blisko");
+                break;
+            case 2:
+                System.out.println("dość blisko");
+                break;
+            default:
+                System.out.println("sprubuj scescia innym razem");
+        }//wyrazenie swich
+        System.out.println(
+                switch (roznica){
+                    case 0 -> "brawo good";
+                    case 1 -> "blisko";
+                    case 2 -> "dość blisko";
+                    default -> "słabo";
+                }
+        );
+        //zgadywanie 10 razy
+        /*
+        for (int i = 0; i < 10; i++) {
+            System.out.println("zgadnij liczbe pruba nr "+(+i+1));
+            wpisanaliczba = klawiatura.nextInt();
+            if (wpisanaliczba == wylosowana){
+                System.out.println("wygrana");
+                break;
+            }else{
+                if (wpisanaliczba>wylosowana){
+                    System.out.println("za duzo");
+                }else {
+                    System.out.println("za mało");
+                }
+         */
+        System.out.println("zgadnij liczbe");
+        wpisanaliczba = klawiatura.nextInt();
+        while (wpisanaliczba != wylosowana)  {
+        if (wpisanaliczba>wylosowana){
+            System.out.println("za duzo");
+        }else {
+            System.out.println("za mało");
+        }
+        System.out.println("zgadnij liczbe");
+        wpisanaliczba = klawiatura.nextInt();
     }
-}
+    }
+    }
